b7805d94dfcbb1e728f1b59af696bdcd05b9f710
Mateo-RENAC <mateo.renac@gmail.com>
2024-04-18 15:36:45 +0200

    WORK IN PROGRESS

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,4 +1,3 @@
-from django.urls import path
 from . import views
 from django.urls import include, path
 
@@ -9,5 +8,5 @@ urlpatterns = [
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
-    path("__debug__/", include("debug_toolbar.urls")),
+    path("__debug__/", include("debug_toolbar.urls"), name="djdt"),
 ]
